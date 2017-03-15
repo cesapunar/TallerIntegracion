a1dc46230bdedfe92db21abdf524fb589e07e4ab
Cristobal Sapunar <cesapunar@uc.cl>
2017-03-14 21:50:24 -0300

    Truncate body when is more than 1000 characters

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
 	validates :down, length: {maximum: 250}
+
+	def body_truncated
+		body.truncate(1003)
+	end
 end
---
+++ db/migrate/20170315001500_remove_link_from_articles.rb
@@ -0,0 +1,5 @@
+class RemoveLinkFromArticles < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :articles, :link, :string
+  end
+end
